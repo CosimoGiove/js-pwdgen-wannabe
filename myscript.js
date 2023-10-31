8895739a5aaec742c14287b41dda4858cd690e8c
cosimogiove <cosimo18497@gmail.com>
2023-10-31 16:41:26 +0100

    feat:lavoro terminato

--- myscript.js
+++ myscript.js
@@ -4,12 +4,14 @@ const nome = document.getElementById("nome")
 const cognome = document.getElementById("cognome")
 const color = document.getElementById("colorepreferito")
 const numero=document.getElementById ("numero")
+const password= document.getElementById("password")
 
 
 nome.innerHTML="il tuo nome:"
 cognome.innerHTML="il tuo cognome:"
 color.innerHTML="il tuo colore preferito:"
-numero.innerHTML = Math.floor(Math.random() * 100);
+password.innerHTML="la tua password:"
+
 console.log(nome);
 console.log(cognome);
 console.log(color);
@@ -18,7 +20,7 @@ console.log(numero);
 const NomeInput = prompt("inserisci il tuo nome");
 const CognomeInput= prompt("inserisci il tuo cognome");
 const ColoreInput= prompt("inserisci il tuo colore preferito");
-
+const casuale =  Math.floor(Math.random() * 100);
 nome.innerHTML += NomeInput;
 console.log(nome.innerHTML)
 
@@ -27,6 +29,8 @@ console.log(cognome.innerHTML)
 
 color.innerHTML+= ColoreInput
 console.log(color.innerHTML)
+
+password.innerHTML += NomeInput + CognomeInput + casuale
 
 
 
